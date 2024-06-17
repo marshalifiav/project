84e758f3e1a4606ff41a58dd079507b46362d67e
marsha <marshaavesthi@gmail.com>
2024-06-17 21:04:03 +0700

    Add mobile app with webview

---
+++ mobile/MobileApp/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <WebView source={{ uri: 'http:// 192.168.1.6:3000' }} style={styles.webview} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default App;\
